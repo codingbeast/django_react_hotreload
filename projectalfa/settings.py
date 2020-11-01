b663447ce141da320e96164420f8b99da31e0ed7
mahakal <advrter@gmail.com>
2020-11-01 13:39:06 +0530

    Revert "ni"
    This reverts commit 8f75825237d7d44754abe0ae4841cef5e53d728c.

--- projectalfa/settings.py
+++ projectalfa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'frontend',
+    'AccountManagement',
     'API',
     'rest_framework',
     'rest_framework.authtoken',
